71d273706a2088560f5543b64343a6a6a79a1daa
Shayne Meyer <shayne.meyer@gmail.com>
2020-02-13 06:28:59 -0800

    added hangman game

---
+++ hangman/readme.md
@@ -0,0 +1,4 @@
+# Hangman game
+Classic Hangman game using vanilla JavaScript and CSS. 
+
+![](docs/hangman.png)
---
+++ hangman/script.js
@@ -0,0 +1,113 @@
+const wordElement = document.getElementById('word');
+const wrongLettersElement = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+// Show hidden word
+function displayWord() {
+  wordElement.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map(
+        letter => `
+      <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+      </span>
+    `
+      )
+      .join('')}
+  `;
+
+  const innerWord = wordElement.innerText.replace(/\n/g, '');
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = 'Congratulations! You won! 😃';
+    popup.style.display = 'flex';
+  }
+}
+
+// Update the wrong letters
+function updateWrongLettersElement() {
+  // Display wrong letters
+  wrongLettersElement.innerHTML = `
+    ${wrongLettersElement.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+  // Display parts
+  figureParts.forEach((part, idx) => {
+    const errors = wrongLetters.length;
+
+    if (idx < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost.';
+    popup.style.display = 'flex';
+  }
+}
+
+// Show notification
+function showNotification() {
+  notification.classList.add('show');
+
+  setTimeout(() => {
+    notification.classList.remove('show');
+  }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener('keydown', e => {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersElement();
+      }
+    }
+  }
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  // Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersElement();
+
+  popup.style.display = 'none';
+});
+
+displayWord();
